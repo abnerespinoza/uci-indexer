b5ff3b253ed55a342fd0745bc4ce19071ea34af8
Abner Espinoza <emrabner@gmail.com>
2021-11-10 19:47:46 -0800

    refactored

--- docIDLookup.py
+++ docIDLookup.py
@@ -1,18 +1,37 @@
 import os
-DEVDIRECTORY = 'DEV'
 
-docIdLookup = {}
-if __name__ == '__main__':
-    #creating documentID lookup
-    folders = os.listdir(DEVDIRECTORY)
-    allFiles = []
-    for folder in folders:
-        files = os.listdir(DEVDIRECTORY + '/' + folder)
-        allFiles.extend(files)
 
-    docId = 1
-    for page in allFiles:
-        docIdLookup[page] = docId  
-        docId += 1
+DEV_DIRECTORY = 'DEV'
+
+
+# for loop below (for folder_name in folders) raised an error when hidden files' names
+#   were appended to DEV_DIRECTORY. Eg .DS_STORE
+#
+# solution: https://stackoverflow.com/questions/7099290/how-to-ignore-hidden-files-using-os-listdir
+def listdir_nohidden(path):
+    f_names = []
+    for f in os.listdir(path):
+        if not f.startswith('.'):
+            f_names.append(f)
+    return f_names
+
+
+docLookup = {}
+def main():
+    # extracting folders from DEV
+    folders = listdir_nohidden(DEV_DIRECTORY)
+
+    # extracting json files from folders
+    json_files = []
+    for folder_name in folders:
+        js = os.listdir(DEV_DIRECTORY + '/' + folder_name) 
+        json_files.extend(js)
+
+    # creating lookup for documents
+    for docID, page in enumerate(json_files):
+        docLookup[docID] = page
     
-    print(docIdLookup)
+    print(docLookup)
+
+if __name__ == '__main__':
+    main()
