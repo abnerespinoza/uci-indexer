d80a82c412162d9f58a13e75e309b2defebf3cad
Abner Espinoza <49922254+abnerespinoza@users.noreply.github.com>
2021-12-03 22:46:41 -0800

    Update buildIndex.py

--- buildIndex.py
+++ buildIndex.py
@@ -222,7 +222,7 @@ def main():
 
 if __name__ == '__main__':
     # DEV folder path
-    DEV_DIRECTORY = 'DEV'
+    DEV_DIRECTORY = 'DEV_no_dups'
 
     # partial_indices folder path
     PARTIAL_INDICES_DIRECTORY = 'partial_indices'
